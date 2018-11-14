1a9048c22a50e57b0d5dc5e8d91c52b3f8990ed6
Santosh Behera <beheras40@gmail.com>
2018-11-14 16:05:38 +0530

    Update heatMap_withSidebar.R

--- heatMap_withSidebar.R
+++ heatMap_withSidebar.R
@@ -2,26 +2,15 @@ library(gplots)
 library(RColorBrewer)
 library(colorspace)
 
-#normalization<-function(x){
-#  dimm=dim(x)
- # for(i in 1:dimm[1]){
-  #  x[i,]=(x[i,]-mean(x[i,]))/sd(x[i,]) 
-  #}
-  #return(x)
-#}
-
 mat_val <- read.csv("Upregulated_genes.txt", sep="\t", header = T, row.names=1)
 data.m <- as.matrix(mat_val)
-data.m
 
-#data.m<-log2(data.m)
-#data.norm<-normalization(data.m)
 
 #myCol <-colorRampPalette(c("red","orange", "yellow","green","blue","darkblue"))(n = 200)
 #myCol <-colorRampPalette(c("khaki1","khaki2","yellow", "orange","red"))(n = 200)
-myCol <-colorRampPalette(c("darkblue","blue","lightblue","white","yellow","red","darkred"))(n = 200)
-myCol2 <-diverge_hcl(12,c=100,l=c(50,90),power=1)
-myCol3<-colorspace::diverge_hsv
+#myCol <-colorRampPalette(c("darkblue","blue","lightblue","white","yellow","red","darkred"))(n = 200)
+#myCol2 <-diverge_hcl(12,c=100,l=c(50,90),power=1)
+#myCol3<-colorspace::diverge_hsv
 myCol4 <-colorRampPalette(c("darkblue","blue","lightblue","yellow","red","darkred"))(n = 200)
 ##myCol <-colorRampPalette(c("Grey","white","black"))(n = 200)
 tiff("delet18.png",units="in",width=20,height=20,res=900)
