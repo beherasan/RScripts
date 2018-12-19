f05dbc619080f8e836b923286bcc6d0eda3aed5e
Santosh Behera <beheras40@gmail.com>
2018-12-19 12:21:56 +0530

    Add files via upload

---
+++ GoChord.R
@@ -0,0 +1,9 @@
+library(GOplot)
+data<-read.csv("chord.csv", header=TRUE, row.names=1)
+data.m<-as.matrix(data)
+data.m
+GOChord(data.m, space = 0.02, gene.order = 'logFC', gene.space = 0.25, gene.size = 5)
+
+
+
+
