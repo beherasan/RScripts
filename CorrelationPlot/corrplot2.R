13024388da2804d94704fe0a3a44e082b3f9b417
Santosh Behera <beheras40@gmail.com>
2020-10-06 15:43:31 +0530

    Create corrplot2.R

---
+++ CorrelationPlot/corrplot2.R
@@ -0,0 +1,7 @@
+##
+
+library(ggpubr)
+data.amygdala<- read.csv("amygdala.txt",header=TRUE, row.names=1,sep="\t")
+ggscatter(data.amygdala,x= "HBP", y= "GTEx",color="HBP",size=1, conf.int=TRUE,cor.coef = TRUE,cor.coef.coord=c(2,4), cor.method = "spearman",xlab="HBP_Amygdala",ylab="GTEx_Amygdala")
+
+##
