a68b009719cb2d60b1e32ee070e6b575ae46b2d6
Santosh Behera <beheras40@gmail.com>
2021-02-04 14:52:52 +0530

    Create corrplot.R

---
+++ CorrelationPlot/corrplot.R
@@ -0,0 +1,16 @@
+
+library(corrplot)
+library(gplots)
+
+mat_val <- read.table("Input_Matrix.csv", header = T, sep="\t", row.names=1)
+data.m <- as.matrix(mat_val)
+
+col <- colorRampPalette(c("white","darkblue","blue", "lightgreen","yellow", "orange","darkorange","red","darkred"))(100)
+corr_mat=cor(data.m)
+
+tiff("corrPlot.png",units="in",width=15,height=15,res=300)
+heatmap.2(corr_mat,col= col,dendrogram ="both",trace='none',margin = c(8,8),keysize = 0.5)
+dev.off()
+
+heatmap(corr_mat,col=col,symm=TRUE)
+corrplot(corr_mat,method="color",order="hclust",col=col,cl.lim=c(-0.6,1))
